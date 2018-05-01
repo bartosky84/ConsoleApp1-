5994b6bcc141e6aab5b06ff4e50d3afcd76cf618
Bartosky Webdeveloper <bartoszpiotrkruk@gmail.com>
2018-05-01 19:15:16 +0200

    Program do obliczania pola prostokąta

--- Program.cs
+++ Program.cs
@@ -10,10 +10,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
          {
-            int valueInt = 4;
-            Console.WriteLine(valueInt);
-            Console.WriteLine("Hello Word czyli Witaj Świecie");
-            Console.ReadKey(true);
+            double a, b, pole;
+            Console.WriteLine("Program oblicza pole prostokąta. ");
+
+            Console.WriteLine("Podaj dlugość boku a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj długość boku b: ");
+            b = double.Parse(Console.ReadLine());
+
+            pole = a * b;
+
+            Console.Write("Pole prostokąta o boku a={0} i boku b={1} ",a,b);
+            Console.Write("Wynosi {0}", pole);
+
+            Console.Read(); //Zamyka Cmd po naciśnieciu Entera
+
+
+
 
         }
     }
